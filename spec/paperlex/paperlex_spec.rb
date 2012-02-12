fb2c8fe99a0e8e955e3ce3398aff6cd94c701040
Ben Woosley <ben.woosley@gmail.com>
2012-02-11 19:58:51 -0800

    Uncover the actual reason for the failure - a test side-effect

--- spec/paperlex/paperlex_spec.rb
+++ spec/paperlex/paperlex_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe Paperlex do
   describe ".live!" do
+    after do
+      Paperlex.base_url = ENV['PAPERLEX_URL']
+    end
+
     it "should point the base_url to the live api endpoint" do
       Paperlex.base_url.should_not == Paperlex::LIVE_URL
       Paperlex.live!
